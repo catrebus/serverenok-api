31f5b4612d38211b251e6b58be3729dd9400f802
catrebus <catrebus@mail.ru>
2026-03-22 21:11:37 +0300

    fix /temperature

--- routers/read_router.py
+++ routers/read_router.py
@@ -7,6 +7,6 @@ read_router = APIRouter(prefix="/read", tags=["read"])
 
 @read_router.get('/temperature')
 async def get_temperature(
-        info_service: SysInfoServiceProtocol = Depends(container.get_info_service)
+        info_service: SysInfoServiceProtocol = Depends(container.info_service)
 ):
     return await info_service.get_temperature()
